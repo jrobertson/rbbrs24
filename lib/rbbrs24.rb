496b1b2b9f965531388ce3a68a7b64e6c04c055b
James Robertson <digital.robertson@gmail.com>
2023-02-24 18:41:41 +0000

    improvement: HTML or CSS can now be easily modified.

--- lib/rbbrs24.rb
+++ lib/rbbrs24.rb
@@ -6,31 +6,57 @@
 #              be executed on the server. Recommended for private use only.
 
 class Rbbrs24
-
-  def initialize(action: 'run')
-    @action = action
-  end
   
-  def render_input()
-%Q(
+  attr_accessor :html_input, :html_output, :css
+
+  def initialize(action: 'run', code: '')
+    
+    @action, @code = action, code
+
+@css = %q(
+form {background-color: #e34}
+textarea {background-color: #eee}
+)
+
+@pg = %q(
 <html>
 <head>
 <style>
-form {background-color: #e34}
-textarea {background-color: #eee}
+$css
 </style>
 </head>
 <body>
-<div>
-  <form action="#{@action}" method="get" target="output">
-    <textarea autofocus="true" name="input1"></textarea>
+$div
+</body>
+</html>
+  )
+  
+@html_input = %q(
+<div class='code'>
+  <form action="$action" method="get" target="output">
+    <textarea autofocus="true" name="input1">$code</textarea>
     <input type="submit" value="run"/>
   </form>
   <iframe name="output" id="output"/>  
 </div>
-</body>
-</html>
-)
+)    
+  end
+  
+  
+  def render_input()
+    @html_input.sub('$action', @action).sub('$code', @code)
+  end
+  
+  def render_input_pg()
+    @pg.sub('$css', @css).sub('$div', render_input())
+  end
+  
+  def render_output()
+    @html_output.sub('$action', @action).sub('$code', code)
+  end  
+  
+  def render_output_pg()
+    @pg.sub('$css', @css).sub('$div', render_input())
   end
 
   def evalx(s)
--- rbbrs24.gemspec
+++ rbbrs24.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'rbbrs24'
-  s.version = '0.1.0'
+  s.version = '0.1.1'
   s.summary = 'Experiment with submitting Ruby code from the browser to be executed on the server. Recommended for private use only.'
   s.authors = ['James Robertson']
   s.files = Dir["lib/rbbrs24.rb"]
